341b99aee7b4333621f16186fe596ead287ace75
Ananyo Bhattacharya <ananyobhattacharya@gmail.com>
2026-04-14 09:43:44 +0530

    create and display a window

--- src/main.c
+++ src/main.c
@@ -4,9 +4,23 @@
 int main(void)
 {
     XGui gui;
-    if (!xgui_init(&gui, 900, 600))
+    if (!xgui_init(&gui, 800, 600))
         return 1;
-    printf("Display opened successfully\n");
+
+    XEvent event;
+    int running = 1;
+
+    while (running)
+    {
+        XNextEvent(gui.display, &event);
+        switch (event.type)
+        {
+        case KeyPress:
+            running = 0;
+            break;
+        }
+    }
+
     xgui_shutdown(&gui);
     return 0;
 }
--- src/xgui.c
+++ src/xgui.c
@@ -13,10 +13,25 @@ bool xgui_init(XGui *gui, int width, int height)
 
     gui->screen = DefaultScreen(gui->display);
 
+    gui->window = XCreateSimpleWindow(
+        gui->display,
+        RootWindow(gui->display, gui->screen), /* parent window */
+        0, 0,                                  /* x, y position */
+        width, height,                         /* size */
+        1,                                     /* border width */
+        BlackPixel(gui->display, gui->screen), /* border color */
+        WhitePixel(gui->display, gui->screen)  /* background color */
+    );
+
+    XSelectInput(gui->display, gui->window, ExposureMask | KeyPressMask | ButtonPressMask);
+
+    XMapWindow(gui->display, gui->window);
+
     return true;
 }
 
 void xgui_shutdown(XGui *gui)
 {
+    XDestroyWindow(gui->display, gui->window);
     XCloseDisplay(gui->display);
 }
